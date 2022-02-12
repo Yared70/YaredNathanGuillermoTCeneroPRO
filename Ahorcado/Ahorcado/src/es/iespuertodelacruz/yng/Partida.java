19c7a32c7a62905657170e53debb9e004d77f408
Yared <yared70.web@gmail.com>
2022-02-12 15:05:41 +0000

    añadido toString faltante

--- Ahorcado/Ahorcado/src/es/iespuertodelacruz/yng/Partida.java
+++ Ahorcado/Ahorcado/src/es/iespuertodelacruz/yng/Partida.java
@@ -14,36 +14,34 @@ import java.util.Random;
  * @author yared
  */
 public class Partida {
-    
+
     private HashMap<Jugador, Jugador> jugadores;
     private Integer puntosTotales;
     private Integer fallos;
     private Integer dificultad;
     private ArrayList<String> historialLetras;
     private String palabraAdivinar;
-    
+
     String palDif1[] = {"angel", "ojo", "pizza", "enojado", "artificiales",
-    "calabaza", "flor", "arco", "barba"};
+        "calabaza", "flor", "arco", "barba"};
     String palDif2[] = {"cerebro", "hebilla", "langosta", "iman", "megafono",
-    "saxofonista", "aspersor", "computadora", "libertad"    
+        "saxofonista", "aspersor", "computadora", "libertad"
     };
     String palDif3[] = {"adaptacion", "evaluacion", "diagnostico", "circulacion",
-    "procedimiento", "trasladar", "ocupacional", "geolocalizacion", "trasnochador"
+        "procedimiento", "trasladar", "ocupacional", "geolocalizacion", "trasnochador"
     };
 
-    
-    
-    public Partida(Jugador p1, Jugador p2){
-    
+    public Partida(Jugador p1, Jugador p2) {
+
         jugadores = new HashMap<Jugador, Jugador>();
         jugadores.put(p1, p2);
         puntosTotales = 0;
         fallos = 0;
         historialLetras = new ArrayList<>();
     }
-    
-    public Partida(Jugador p1, Integer dificultad){
-        
+
+    public Partida(Jugador p1, Integer dificultad) {
+
         Random rnd = new Random();
         jugadores = new HashMap<Jugador, Jugador>();
         jugadores.put(p1, null);
@@ -51,74 +49,89 @@ public class Partida {
         puntosTotales = 0;
         fallos = 0;
         historialLetras = new ArrayList<>();
-        if(dificultad == 1){
+        if (dificultad == 1) {
             int aleatorio = rnd.nextInt(palDif1.length);
             palabraAdivinar = palDif1[aleatorio];
-        }else if(dificultad == 2){
+        } else if (dificultad == 2) {
             int aleatorio = rnd.nextInt(palDif2.length);
             palabraAdivinar = palDif2[aleatorio];
-        }else{
+        } else {
             int aleatorio = rnd.nextInt(palDif3.length);
             palabraAdivinar = palDif3[aleatorio];
         }
-        
+
     }
-    
-    public Integer calcularPuntos(Integer fallos){
-        
+
+    public Integer calcularPuntos(Integer fallos) {
+
         Integer resultado = 0;
-        
-        if (fallos == 6){
+
+        if (fallos == 6) {
             return resultado;
-        }else if(dificultad == 1){
-            resultado = 120 - (20*fallos);
+        } else if (dificultad == 1) {
+            resultado = 120 - (20 * fallos);
             return resultado;
-        }else if(dificultad == 2){
-            resultado = 120 - (15*fallos);
+        } else if (dificultad == 2) {
+            resultado = 120 - (15 * fallos);
             return resultado;
-        }else{
-            resultado = 120 - (10*fallos);
+        } else {
+            resultado = 120 - (10 * fallos);
             return resultado;
         }
-        
-        
+
     }
-    
-    public void elegirPalabra(String palabra){
-        
+
+    public void elegirPalabra(String palabra) {
+
         palabraAdivinar = palabra;
-        
-        
+
     }
 
     public ArrayList<String> getHistorialLetras() {
         return historialLetras;
     }
-    
-    public void addLetra(String letra){
-        
+
+    public void addLetra(String letra) {
+
         historialLetras.add(letra);
-        
+
     }
 
     public String getPalabraAdivinar() {
         return palabraAdivinar;
     }
-    
-    public String palabraGuiones(){
-        
-    String palabra = this.palabraAdivinar;
-    String letraselegidas = "";
+
+    public String palabraGuiones() {
+
+        String palabra = this.palabraAdivinar;
+        String letraselegidas = "";
         for (int i = 0; i < historialLetras.size(); i++) {
             letraselegidas += historialLetras.get(i);
         }
 
-    String palabraconguiones = palabra.replaceAll("[^ "+letraselegidas+"]", "_ ");
-    
-    return palabraconguiones;
-    
-  }
+        String palabraconguiones = palabra.replaceAll("[^ " + letraselegidas + "]", "_ ");
 
+        return palabraconguiones;
+
+    }
     
-    
+    @Override
+    public String toString(){
+        
+        String respuesta = "";
+        
+        if(jugadores.values() == null){
+            respuesta += "Jugador: " + jugadores.keySet() + "\tDificultad: " +
+                    dificultad + "\tPalabra a adivinar: " + palabraAdivinar + 
+                    "\tFallos: " + fallos + "\tPuntos totales: " + puntosTotales;
+        
+        }else{
+            respuesta += "Jugador1: " + jugadores.keySet() + "Jugador2: " + jugadores.values() +
+                    "\tDificultad: " + dificultad + "\tPalabra a adivinar: " + palabraAdivinar + 
+                    "\tFallos: " + fallos + "\tPuntos totales: " + puntosTotales;
+
+        }
+        return respuesta;
+    }
+
 }
